1ecb9b0b63e4dd48c8e12c915aad236b0ca48bb7
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-17 18:19:42 -0400

    test api request

--- src/pages/Starships.jsx
+++ src/pages/Starships.jsx
@@ -1,11 +1,25 @@
-import React from "react"
-
+import React, { useState, useEffect } from "react"
+import { getAllStarships } from "../services/sw-api"
+import { Link } from 'react-router-dom'
 
 const Starships = (props) => {
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    getAllStarships()
+      .then(starships => setResults(starships.results))
+  }, [])
+
   return (
     <>
       <h3>All Starships</h3>
-
+      <div className="starship-container">
+        {results.map((starshipTitle) => (
+          <Link key={starshipTitle.index} state={{ starshipTitle }} to='/starship'>
+            {starshipTitle.name}
+          </Link>
+        ))}
+      </div>
     </>
   )
 }
